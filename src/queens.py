2dbf1e472cf9fa715d02e9df5667a363f603d118
Jim Holmström <jim.holmstroem@gmail.com>
2013-01-12 12:47:02 +0100

    save

--- src/queens.py
+++ src/queens.py
@@ -1,10 +1,11 @@
 import random
 import numpy as np
-
+import itertools as it
 
 class queen_generator:
-    def __init__(self,N):
+    def __init__(self, N, dim = 2):
         self.N = N
+        self.dim = dim
         self.constraints = [] 
         def no_self_connection(state):
             """docstring for no_self_connection"""
@@ -20,17 +21,46 @@ class queen_generator:
         self.constraints.append(no_self_junction_connection)
 
     def __iter__(self):
-        for solution in self.solutions(): #Just passing on the yield
+        for solution in self.solutions(self.empty_board()): #Just passing on the yield
             yield solution
 
-    def solutions(self,state=None):
-        """docstring for solutions"""
-        if(state is None):
-            state = np.zeros((self.N,self.N))
-        if(np.sum(np.sum(state))==self.N):
-            yield state
-        else
-            
+    def solutions(self, state):
+        """docstring for solutions
+        
+        yields solutions 
+        """
+        for new_state in queen_generator.valid_states( state ):
+            num_queens = np.sum(np.sum(new_state))
+            if( num_queens < self.N ):
+                self.solutions(new_state)
+            elif( num_queens == self.N ):
+                yield new_state
+            else:
+                raise Exception(
+                    "Too many queens on the field, \
+                    something is wrong with the algorithm"
+                    )
+
+    @staticmethod
+    def valid_states( state ):
+        """docstring for valid_coordinates"""
+        pass 
+
+    @staticmethod
+    def iterate_empty_coordinates( state ):
+        """docstring for iterate_empty_coordinates
+
+        iterates tuple (x,y) for all coordintes which are free
+        """
+        coords = map(lambda axis:
+                    np.nonzero(state.any(axis=axis))[0],
+                    range(dim)
+                )
+    
+        return it.product(*coords)
+
+    def empty_board(self):
+        return np.zeros((self.N,self.N),dtype=bool)
 
 for s in queen_generator(5):
     print s
