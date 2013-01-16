3851ddb2aa048faa63908af8214ada02f2b660de
Jim Holmström <jim.holmstroem@gmail.com>
2013-01-16 18:00:02 +0100

    added things

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import operator
+import random
 
 observation_missrate = 0.05
 
@@ -9,6 +10,11 @@ class Graph(object):
     
     def __init__(self, matrix):
         self.matrix = matrix
+
+    def adjacent_connections(self, v_from):
+        # Obtain other connections
+        return [ (index, connection) for (index, connection)
+                        in enumerate(self.matrix[v_from]) if connection != 0 ]
 
     def c(self, v_from, e, t, o, p = observation_missrate):
         v_to = e[0]
@@ -21,10 +27,7 @@ class Graph(object):
         if e[1] not in self.matrix[v_from][v_to]:
             raise IndexError()
         
-        # Obtain other connections
-        connections = [ (index, connection) for (index, connection)
-                        in enumerate(self.matrix[v_from]) if connection != 0 ]
-
+        connections = self.adjacent_connections( v_from )
         other_connections = [ (index, tp) for (index, connection) in connections
                               for tp in connection if (index, tp) != e ]
         
@@ -40,10 +43,58 @@ class Graph(object):
             f = [ (v_from, edge_prev) for (v_from, edge_prev) in other_connections if edge_prev == Graph.O ][0]
             
             return probability * self.c(f[0], (v_from, f[1]), t - 1, o)
+
+# p(s, O|sigma,G)
+def state_probability(graph, v_from, e, o, sigma):
+    return reduce(operator.mul, [ graph.c(v_from, e, i, o) for i in range(len(o)) ] )
+
+# p(sigma|O,G)
+def sigma_probability(sigma, o, graph, v_from, e):
+    return state_probability(graph, v_from, e, o, sigma)
+
+# p(sigma|O,G)
+def MCMC(graph, o, sampling_burn_in, sampling_interval, number_of_samples)
+    """docstring for MCMC
+    returns a sample from the p(sigma|O,G)
+    """
+    #v_from = random.randint(0, len(graph.matrix) - 1)
+    #e = ( random.randint(0, len(graph.matrix) - 1), random.choice([ Graph.L, Graph.R, Graph.O ]) )
+
+    #FIXME: number_of_samples
+
+    for v_from in range(len(graph.matrix)):
+        for e in graph.adjacent_connections(v_from):
+            # Randomize start sigma
+            sigma = random.getrandbits(len(graph.matrix))
+            sigma_prob = sigma_probability(sigma, o, graph, v_from, e)
+
+            i = 0
+
+            while True:
+                sigma_p = sigma ^ (1 << random.randint(0, len(graph.matrix) - 1))
+                sigma_prob_p = sigma_probability(sigma_p, o, graph, v_from, e)
+
+                alpha = sigma_prob_p / sigma_prob
+
+                if alpha > 1 or random.random() <= alpha:
+                    sigma, sigma_prob = sigma_p, sigma_prob_p
+
+                    # Sample
+                    if i % sampling_interval = 0 and i / sampling_interval > sampling_burn_in:
+                        yield sigma_p
+
+                i += 1
+
+# p(O|sigma,G)
+def observation_probability(graph,sigma,o):
+    """docstring for observation_probability
     
-    # p(s, O|G, sigma)
-    def state_probability(self, v_from, e, o, sigma):
-        return reduce(operator.mul, [ self.c(v_from, e, i, o) for i in range(len(o)) ] )
+    """
+    return sum(
+        [graph.c(v_from,e,len(o)-1) 
+            for v_from in range(len(graph.matrix)) 
+            for e in graph.adjacent_connections(v_from)]
+        )
 
 if __name__ == "__main__":
     import sys
